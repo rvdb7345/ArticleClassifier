c6a981b663def2847c5c5df4c723a2e313875368
Robin <rvdb7345@gmail.com>
2023-02-26 19:08:14 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project to use citation graph networks to improve article classification',
+    author='Robin van den Berg',
+    license='MIT',
+)
