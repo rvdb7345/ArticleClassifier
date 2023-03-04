bde9694f1fd9583ff56a3667ed100ec5e086963c
rvdb7345 <rvdb7345@gmail.com>
2023-03-04 18:28:42 +0100

    Some first analysis of the canary dataset.

---
+++ src/data/data_loader.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+class DataLoader():
+    """Class for loading all the necessary data."""
+    def __init__(self, data_locs: dict):
+        self.train_loc = data_locs['train']
+        self.val_loc = data_locs['val']
+        self.test_loc = data_locs['test']
+
+    def load_train_data(self):
+        train_data = pd.read_csv(self.train_loc)
+        return train_data
+
+    def load_val_data(self):
+        val_loc = pd.read_csv(self.val_loc)
+        return val_loc
+
+    def load_test_data(self):
+        test_loc = pd.read_csv(self.test_loc)
+        return test_loc
+
