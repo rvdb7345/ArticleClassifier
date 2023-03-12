b3c6a39f111420cceddfe47271c2ae02de7b32da
rvdb7345 <rvdb7345@gmail.com>
2023-03-12 14:29:23 +0100

    Added loading for the parsed xml csv.

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -7,21 +7,28 @@ class DataLoader():
         self.val_loc = data_locs['val']
         self.test_loc = data_locs['test']
         self.xml_loc = data_locs['xml']
+        self.xml_csv_loc = data_locs['xml_csv']
 
     def load_train_data(self):
         train_data = pd.read_csv(self.train_loc)
         return train_data
 
     def load_val_data(self):
-        val_loc = pd.read_csv(self.val_loc)
-        return val_loc
+        val_data = pd.read_csv(self.val_loc)
+        return val_data
 
     def load_test_data(self):
-        test_loc = pd.read_csv(self.test_loc)
-        return test_loc
+        test_data = pd.read_csv(self.test_loc)
+        return test_data
 
     def load_xml_data(self):
         xml_df = pd.read_xml(self.xml_loc)
 
         return xml_df
+
+    def load_xml_csv(self):
+        xml_csv = pd.read_csv(self.xml_csv_loc)
+        return xml_csv
+
+
 
