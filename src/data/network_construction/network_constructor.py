b951113873f405fc67bc8a3951b5df8623e6b208
rvdb7345 <rvdb7345@gmail.com>
2023-03-25 17:19:56 +0100

    Added documentation to the network construction.

--- src/data/network_construction/network_constructor.py
+++ src/data/network_construction/network_constructor.py
@@ -15,16 +15,35 @@ class NetworkConstructor():
         assert False, f'Explosion function not defined for {self.network_type}'
         pass
 
-    def generate_network(self):
+    def generate_network(self) -> nx.Graph:
+        """
+        Generate network x graph for the data.
+
+        Returns:
+            A network with the articles as nodes
+        """
+
+        # create individual row for each item to link on
         exploded_df = self.explode_to_single_item_per_row(self.data_df)
+
+        # create groups that should be linked
         grouped_df = exploded_df[['pui', self.link_variable]].groupby(self.link_variable)
+
+        # get lists of the links that should exists (two-itemed combinations of the groups)
         grouped_all_combinations_df = grouped_df.apply(lambda x: list(itertools.combinations(x['pui'].tolist(), 2)))
+
+        # remove isolated items
         grouped_all_combinations_not_empty_df = grouped_all_combinations_df[
             grouped_all_combinations_df.map(lambda d: len(d)) > 0]
+
+        # get individual rows for each link
         all_links_df = grouped_all_combinations_not_empty_df.to_frame().explode(0)
+
+        # compose edge list
         all_links_df[['from', 'to']] = pd.DataFrame(all_links_df[0].tolist(), index=all_links_df.index)
         all_links_df.drop(0, inplace=True, axis=1)
+
+        # create a networkx graph from the edge list that was just constructed
         networkx_graph = nx.from_pandas_edgelist(all_links_df.iloc[:3000000], source='from', target='to')
 
-        print(all_links_df)
         return networkx_graph
