68837e77fe071fb9e3acc4826c9e47be9aee10f1
rvdb7345 <rvdb7345@gmail.com>
2023-03-25 15:56:34 +0100

    Added key checks for loading fucntions.

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -7,27 +7,39 @@ class DataLoader():
         self.data_locs = data_locs
 
     def load_train_data(self):
+        assert 'train' in self.data_locs.keys(), \
+            f'Cannot load train data as path is not given, only paths for {self.data_locs.keys()}'
         train_data = pd.read_csv(self.data_locs['train'])
         return train_data
 
     def load_val_data(self):
+        assert 'val' in self.data_locs.keys(), \
+            f'Cannot load val data as path is not given, only paths for {self.data_locs.keys()}'
         val_data = pd.read_csv(self.data_locs['val'])
         return val_data
 
     def load_test_data(self):
+        assert 'test' in self.data_locs.keys(), \
+            f'Cannot load test data as path is not given, only paths for {self.data_locs.keys()}'
         test_data = pd.read_csv(self.data_locs['test'])
         return test_data
 
     def load_xml_data(self):
+        assert 'xml' in self.data_locs.keys(), \
+            f'Cannot load xml data as path is not given, only paths for {self.data_locs.keys()}'
         xml_df = pd.read_xml(self.data_locs['xml'])
 
         return xml_df
 
     def load_xml_csv(self):
+        assert 'xml_csv' in self.data_locs.keys(), \
+            f'Cannot load xml_csv data as path is not given, only paths for {self.data_locs.keys()}'
         xml_csv = pd.read_csv(self.data_locs['xml_csv'])
         return xml_csv
 
     def load_processed_csv(self):
+        assert 'processed_csv' in self.data_locs.keys(), \
+            f'Cannot load processed_csv data as path is not given, only paths for {self.data_locs.keys()}'
         processed_csv = pd.read_csv(self.data_locs['processed_csv'])
         return processed_csv
 
