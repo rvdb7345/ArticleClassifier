17bd6e76a40db5665397509d0a02917f517215cf
rvdb7345 <rvdb7345@gmail.com>
2023-04-06 22:06:05 +0200

    Added dualGAT and dualGCN as well as simultaneous network prep.

--- src/models/train_gcn.py
+++ src/models/train_gcn.py
@@ -10,11 +10,9 @@ from typeguard import typechecked
 import networkx
 import torch
 from torch_geometric.data import Data
-from torch_geometric.nn import GCNConv
 from torch_geometric.utils.convert import from_networkx
 from sklearn.model_selection import train_test_split
 
-import torch.nn.functional as F
 from tqdm import tqdm
 
 sys.path.append(
@@ -29,60 +27,14 @@ sys.path.append(gv.PROJECT_PATH)
 
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
-from torch_geometric.nn import GATConv
 
-
-class GAT(torch.nn.Module):
-    def __init__(self, hidden_channels, num_features, num_labels, heads):
-        super().__init__()
-        torch.manual_seed(1234567)
-        self.conv1 = GATConv(num_features, hidden_channels, heads)
-        self.conv2 = GATConv(heads * hidden_channels, hidden_channels, heads)
-        self.conv3 = GATConv(heads * hidden_channels, hidden_channels, heads)
-        self.convend = GATConv(heads * hidden_channels, num_labels, 1)
-
-    def forward(self, x, edge_index):
-        x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv1(x, edge_index)
-        x = F.elu(x)
-        x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv2(x, edge_index)
-        x = F.elu(x)
-        x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv3(x, edge_index)
-        x = F.elu(x)
-        x = F.dropout(x, p=0.2, training=self.training)
-        x = self.convend(x, edge_index)
-
-        return torch.sigmoid(x)
-
-
-class GCN(torch.nn.Module):
-    def __init__(self, hidden_channels, num_features, num_labels):
-        super().__init__()
-        torch.manual_seed(1234567)
-        self.conv1 = GCNConv(num_features, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, hidden_channels)
-        self.conv3 = GCNConv(hidden_channels, hidden_channels)
-        self.convend = GCNConv(hidden_channels, num_labels)
-
-    def forward(self, x, edge_index):
-        x = self.conv1(x, edge_index)
-        x = x.relu()
-        x = F.dropout(x, p=0.1, training=self.training)
-        x = self.conv2(x, edge_index)
-        x = x.relu()
-        x = F.dropout(x, p=0.1, training=self.training)
-        x = self.conv3(x, edge_index)
-        x = x.relu()
-        x = F.dropout(x, p=0.1, training=self.training)
-        x = self.convend(x, edge_index)
-
-        return torch.sigmoid(x)
-
+from src.models.graph_network_models.single_stream_gat import GAT
+from src.models.graph_network_models.single_stream_gcn import GCN
+from src.models.graph_network_models.dual_stream_gcn import dualGCN
+from src.models.graph_network_models.dual_stream_gat import dualGAT
 
 @typechecked
-def train(model: torch.nn.Module, data: Data, optimizer, criterion):
+def train(model: torch.nn.Module, data: list[Data], optimizer, criterion):
     """
     Perform one training iteration of the model.
 
@@ -95,17 +47,19 @@ def train(model: torch.nn.Module, data: Data, optimizer, criterion):
     Returns:
 
     """
+    data_inputs = [d for data_object in data for d in (data_object.x, data_object.edge_index)]
+
     model.train()
     optimizer.zero_grad()
-    out = model(data.x, data.edge_index)
-    loss = criterion(out[data.train_mask], data.y[data.train_mask])
+    out = model(*data_inputs)
+    loss = criterion(out[data[0].train_mask], data[0].y[data[0].train_mask])
     loss.backward()
     optimizer.step()
     return loss
 
 
 @typechecked
-def evaluate_metrics(model: torch.nn.Module, data: Data, dataset: str = 'test') -> dict:
+def evaluate_metrics(model: torch.nn.Module, data: list[Data], dataset: str = 'test') -> dict:
     """
     Calculate the different metrics for the specified dataset.
 
@@ -118,16 +72,19 @@ def evaluate_metrics(model: torch.nn.Module, data: Data, dataset: str = 'test') 
         Dictionary with the metrics
     """
     if dataset == 'test':
-        mask = data.test_mask
+        mask = data[0].test_mask
     elif dataset == 'train':
-        mask = data.train_mask
+        mask = data[0].train_mask
     else:
         assert False, f'Dataset {dataset} not recogined. Should be "train" or "test".'
     model.eval()
-    out = model(data.x, data.edge_index)
+
+    data_inputs = [d for data_object in data for d in (data_object.x, data_object.edge_index)]
+
+    out = model(*data_inputs)
     pred = out
 
-    metric_calculator = Metrics(pred[mask].detach().numpy(), data.y[mask].detach().numpy(),
+    metric_calculator = Metrics(pred[mask].detach().numpy(), data[0].y[mask].detach().numpy(),
                                 threshold=0.5)
     metrics = metric_calculator.retrieve_all_metrics()
 
@@ -202,8 +159,52 @@ def get_mask(index: list, size: int) -> torch.Tensor:
     return mask
 
 
+def make_torch_network(sampled_graph, embedding_df, processed_df, train_mask, test_mask, node_label_mapping):
+
+
+    # set the node attributes (abstracts and labels) in the networkx graph for consistent processing later on
+    networkx.set_node_attributes(sampled_graph,
+                                 dict(zip(embedding_df.loc[embedding_df['pui'].isin(sampled_graph.nodes),
+                                                           'pui'].astype(str).to_list(),
+                                          embedding_df.loc[embedding_df['pui'].isin(sampled_graph.nodes),
+                                                           embedding_df.columns.difference(['pui'])].astype(
+                                              np.float32).to_numpy())), 'x')
+    networkx.set_node_attributes(sampled_graph,
+                                 dict(zip(processed_df.loc[processed_df['pui'].isin(sampled_graph.nodes),
+                                                           'pui'].astype(str).to_list(),
+                                          label_columns.loc[label_columns['pui'].isin(sampled_graph.nodes),
+                                                            label_columns.columns.difference(['pui'])].astype(
+                                              np.float32).to_numpy())), 'y')
+
+    # would be nice if this one works, but it sadly doesn't
+    # pyg_graph = from_networkx(sampled_graph)
+
+    # x = embedding_df.loc[
+    #         embedding_df['pui'].isin(sampled_graph.nodes), embedding_df.columns.difference(['pui'])].astype(np.float32).to_numpy()
+    # y = label_columns.loc[
+    #         label_columns['pui'].isin(sampled_graph.nodes), label_columns.columns.difference(['pui'])].astype(np.uint8).to_numpy()
+
+    # set the ids at incremental integers.
+    sampled_graph = networkx.relabel_nodes(sampled_graph, node_label_mapping)
+
+    # get the x and the y from the networkx graph
+    # TODO: enforce order of list for piece of mind
+    x = np.array([emb['x'] for (u, emb) in sampled_graph.nodes(data=True)])
+    y = np.array([emb['y'] for (u, emb) in sampled_graph.nodes(data=True)])
+
+    # create the torch data object for further training
+    data = Data(x=torch.from_numpy(x),
+                edge_index=torch.from_numpy(np.array(sampled_graph.edges(data=False), dtype=np.int32).T),
+                y=torch.from_numpy(y),
+                train_mask=train_mask,
+                test_mask=test_mask)
+
+    return data
+
+
+
 if __name__ == '__main__':
-    gnn_type = 'GAT'
+    gnn_type = 'dualGAT'
 
     # load all the data
     loc_dict = {
@@ -224,97 +225,77 @@ if __name__ == '__main__':
         embedding_df[embedding_df.columns.difference(['pui'])].std()
 
     author_networkx = data_loader.load_author_network()
+    keyword_network = data_loader.load_keyword_network()
 
     # process the labels we want to select now
 
-    label_columns = processed_df.loc[:, ~processed_df.columns.isin(
-        ['file_name', 'title', 'keywords', 'abstract', 'abstract_2', 'authors', 'organization', 'chemicals',
-         'num_refs', 'date-delivered', 'labels_m', 'labels_a'])]
+    # label_columns = processed_df.loc[:, ~processed_df.columns.isin(
+    #     ['file_name', 'title', 'keywords', 'abstract', 'abstract_2', 'authors', 'organization', 'chemicals',
+    #      'num_refs', 'date-delivered', 'labels_m', 'labels_a'])]
 
-    # label_columns = processed_df.loc[:, ['pui', 'human', 'mouse', 'rat', 'nonhuman',
-    #                                      'controlled study', 'animal experiment']]
+    label_columns = processed_df.loc[:, ['pui', 'human', 'mouse', 'rat', 'nonhuman',
+                                         'controlled study', 'animal experiment']]
     label_columns[label_columns.columns.difference(['pui'])] = label_columns[
         label_columns.columns.difference(['pui'])].astype(int)
     features = ['file_name', 'pui', 'title', 'keywords', 'abstract', 'abstract_2', 'authors', 'organization',
                 'chemicals',
                 'num_refs', 'date-delivered', 'labels_m', 'labels_a']
 
+    subsample_size = 10000
+
+    # drop all nodes that do not have an embedding
+    # nodes_to_remove = []
+    # for network in [author_networkx, keyword_network]:
+    #     nodes_to_remove += [node for node in list(network.nodes) if not node in embedding_df.pui.to_list()]
+    #
+    # for node in nodes_to_remove:
+    #     for network in [author_networkx, keyword_network]:
+    #         network.remove_node(node)
+
+    available_nodes = list(set(author_networkx.nodes) & set(keyword_network.nodes) & set(embedding_df.pui.to_list()))
+    sampled_nodes = random.sample(available_nodes, subsample_size)
+    sampled_author = author_networkx.subgraph(sampled_nodes).copy()
+    sampled_keyword = keyword_network.subgraph(sampled_nodes).copy()
+
+    # nodes can only have incremental integers as labels, so we create a mapping to remember which pui is which idx
+    node_label_mapping = dict(zip(sampled_author.nodes, range(len(sampled_author))))
+
+    del author_networkx
+    del keyword_network
+
     # initiate the model
     if gnn_type == 'GCN':
-        model = GCN(hidden_channels=16, num_features=embedding_df.shape[1]-1, num_labels=len(label_columns.columns) - 1)
+        model = GCN(hidden_channels=32, num_features=embedding_df.shape[1]-1, num_labels=len(label_columns.columns) - 1)
     elif gnn_type == 'GAT':
-        model = GAT(hidden_channels=16, num_features=embedding_df.shape[1]-1, num_labels=len(label_columns.columns)-1,
+        model = GAT(hidden_channels=32, num_features=embedding_df.shape[1]-1, num_labels=len(label_columns.columns)-1,
+                    heads=8)
+    elif gnn_type == 'dualGCN':
+        model = dualGCN(hidden_channels=32, num_features=embedding_df.shape[1]-1, num_labels=len(label_columns.columns)-1)
+    elif gnn_type == 'dualGAT':
+        model = dualGAT(hidden_channels=32, num_features=embedding_df.shape[1]-1, num_labels=len(label_columns.columns)-1,
                     heads=8)
     else:
-        assert False, f'Model type: {gnn_type} not recognised, must be in: ["GCN", "GAT"]'
+        assert False, f'Model type: {gnn_type} not recognised, must be in: ["GCN", "GAT", "dualGCN", "dualGAT"]'
+
 
     model.eval()
 
-    # subsample the graph for less computational load
-    k = 10000
-    sampled_nodes = random.sample(author_networkx.nodes, k)
-    sampled_graph = author_networkx.subgraph(sampled_nodes).copy()
-    del author_networkx
-
-    # set the node attributes (abstracts and labels) in the networkx graph for consistent processing later on
-    networkx.set_node_attributes(sampled_graph,
-                                 dict(zip(embedding_df.loc[embedding_df['pui'].isin(sampled_graph.nodes),
-                                                           'pui'].astype(str).to_list(),
-                                          embedding_df.loc[embedding_df['pui'].isin(sampled_graph.nodes),
-                                                           embedding_df.columns.difference(['pui'])].astype(
-                                              np.float32).to_numpy())), 'x')
-    networkx.set_node_attributes(sampled_graph,
-                                 dict(zip(processed_df.loc[processed_df['pui'].isin(sampled_graph.nodes),
-                                                           'pui'].astype(str).to_list(),
-                                          label_columns.loc[label_columns['pui'].isin(sampled_graph.nodes),
-                                                            label_columns.columns.difference(['pui'])].astype(
-                                              np.float32).to_numpy())), 'y')
-
-    # would be nice if this one works, but it sadly doesn't
-    # pyg_graph = from_networkx(sampled_graph)
-
-    # drop all nodes that do not have an embedding
-    nodes_to_remove = [node for node in list(sampled_graph.nodes) if not node in embedding_df.pui.to_list()]
-    for node in nodes_to_remove:
-        sampled_graph.remove_node(node)
-
-    # nodes can only have incremental integers as labels, so we create a mapping to remember which pui is which idx
-    node_label_mapping = dict(zip(sampled_graph.nodes, range(len(sampled_graph))))
-
-    # x = embedding_df.loc[
-    #         embedding_df['pui'].isin(sampled_graph.nodes), embedding_df.columns.difference(['pui'])].astype(np.float32).to_numpy()
-    # y = label_columns.loc[
-    #         label_columns['pui'].isin(sampled_graph.nodes), label_columns.columns.difference(['pui'])].astype(np.uint8).to_numpy()
-
-    # set the ids at incremental integers.
-    sampled_graph = networkx.relabel_nodes(sampled_graph, node_label_mapping)
-
-    # get the x and the y from the networkx graph
-    # TODO: enforce order of list for piece of mind
-    x = np.array([emb['x'] for (u, emb) in sampled_graph.nodes(data=True)])
-    y = np.array([emb['y'] for (u, emb) in sampled_graph.nodes(data=True)])
-
     # create train and test split
-    train_indices, test_indices = train_test_split(range(len(x)), test_size=0.2, random_state=0)
-    train_mask = get_mask(train_indices, len(x))
-    test_mask = get_mask(test_indices, len(x))
-
-    # create the torch data object for further training
-    data = Data(x=torch.from_numpy(x),
-                edge_index=torch.from_numpy(np.array(sampled_graph.edges(data=False), dtype=np.int32).T),
-                y=torch.from_numpy(y),
-                train_mask=train_mask,
-                test_mask=test_mask)
+    train_indices, test_indices = train_test_split(range(len(sampled_author)), test_size=0.2, random_state=0)
+    train_mask = get_mask(train_indices, len(sampled_author))
+    test_mask = get_mask(test_indices, len(sampled_author))
+    author_data = make_torch_network(sampled_author, embedding_df, processed_df, train_mask, test_mask, node_label_mapping)
+    keyword_data = make_torch_network(sampled_keyword, embedding_df, processed_df, train_mask, test_mask, node_label_mapping)
 
     # print(data.edge_index.detach().numpy().shape)
     # assert False
     # get the output of an untrained model
-    out = model(data.x, data.edge_index)
-    print('The predictions of the test set before training: ', out[data.test_mask].detach().numpy())
+    out = model(author_data.x, author_data.edge_index, keyword_data.x, keyword_data.edge_index)
+    print('The predictions of the test set before training: ', out[author_data.test_mask].detach().numpy())
     # visualize(out, color=data.y.argmax(dim=1))
 
     # set training parameters
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-3)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-3)
     criterion = torch.nn.BCELoss()
 
     # train model
@@ -322,8 +303,9 @@ if __name__ == '__main__':
     test_acc_all = []
     loss_all = []
 
-    num_epochs = 100
+    num_epochs = 20
     plot_metric = "Macro F1 score"
+    data = [author_data, keyword_data]
     for epoch in (pbar := tqdm(range(1, num_epochs))):
         loss = train(model, data, optimizer, criterion)
 
@@ -345,4 +327,4 @@ if __name__ == '__main__':
     evaluate_metrics(model, data, dataset='test')
     # print(f'Test Accuracy: {test_acc:.4f}')
 
-    # visualize(out, color=data.y.argmax(dim=1))
+    visualize(out, color=data.y.argmax(dim=1))
