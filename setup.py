e0d7f1702ea1e96d46fc1ee85a9351e7070e3dae
rvdb7345 <rvdb7345@gmail.com>
2023-06-20 15:13:52 +0200

    Rename

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Project to use citation graph networks to improve article classification',
+    description='Project to use graph neural networks and label-guided embeddings to improve article classification',
     author='Robin van den Berg',
     license='MIT',
 )
